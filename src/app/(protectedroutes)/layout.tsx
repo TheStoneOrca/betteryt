af0129e1d0bfe067d2fed5c055086d16a983738b
TheStoneOrca <144631439+TheStoneOrca@users.noreply.github.com>
2024-02-16 12:32:31 -0500

    Added protected rendering so unsignedin users will be redirected

---
+++ src/app/(protectedroutes)/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useEffect } from "react";
+import useUser from "../hooks/useuserhook";
+import { useRouter } from "next/navigation";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const { isSignedIn, isLoaded } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+    if (!isSignedIn) {
+      router.push("/signup");
+    }
+  }, [isLoaded]);
+  return <div>{children};</div>;
+}
